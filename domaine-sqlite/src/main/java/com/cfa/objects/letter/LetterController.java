a3841387d6418991f09ae066131e521a95f0d242
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2022-05-26 13:27:53 +0200

    TME1

--- domaine-sqlite/src/main/java/com/cfa/objects/letter/LetterController.java
+++ domaine-sqlite/src/main/java/com/cfa/objects/letter/LetterController.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -13,15 +12,23 @@ import java.util.List;
 @Slf4j
 @AllArgsConstructor
 public class LetterController {
+
+    private final LetterService letterService;
+
     @GetMapping("/")
     public List<Letter> getAll(@RequestParam(required = false) String message,
                                @RequestParam(required = false) Date creationDate,
-                               @RequestParam(required = false) Date treatmentDate){
-        return new ArrayList<>();
+                               @RequestParam(required = false) Date treatmentDate) {
+        return letterService.getAllLetters(message, creationDate, treatmentDate);
     }
 
     @GetMapping("/{id}")
-    public Letter getById(@PathVariable("id") long id){
-        return null;
+    public Letter getById(@PathVariable("id") long id) {
+        return letterService.getLetterById(id);
+    }
+
+    @PostMapping("/{id}")
+    public long saveLetter(@RequestBody Letter letter) {
+        return letterService.saveLetter(letter);
     }
 }
