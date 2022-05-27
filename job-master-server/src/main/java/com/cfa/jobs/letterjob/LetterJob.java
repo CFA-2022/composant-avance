eae481aa770046f2b40fb824555a185a2676a299
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2022-05-27 13:14:26 +0200

    Clean project

--- job-master-server/src/main/java/com/cfa/jobs/letterjob/LetterJob.java
+++ job-master-server/src/main/java/com/cfa/jobs/letterjob/LetterJob.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableBatchIntegration
